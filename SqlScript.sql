248fa592e2d1ec770355c41ad3285ec91586b7dc
Minh Vo <18120211>
2021-06-12 16:42:23 +0700

    Update GUI

--- SqlScript.sql
+++ SqlScript.sql
@@ -4,121 +4,146 @@ create database course_registration_system;
 
 use course_registration_system;
 
-create table MINISTRY(
-	ID int auto_increment,
-    USERNAME varchar(30),
-    PASSWORD varchar(30),
-    NAME nvarchar(100),
-	constraint PK_MT primary key(ID)
+create table class
+(
+    ID        int auto_increment
+        primary key,
+    NAME      varchar(100) charset utf8 null,
+    NUMMALE   int default 0             null,
+    NUMFEMALE int default 0             null
 );
 
-create table SUBJECT(
-	ID int auto_increment,
-    CODE char(10),
-    NAME nvarchar(100),
-    NUMCREDIT int,
-    constraint PK_SJ primary key(ID)
+create table ministry
+(
+    ID       int auto_increment
+        primary key,
+    USERNAME varchar(30)               null,
+    PASSWORD varchar(30)               null,
+    NAME     varchar(100) charset utf8 null
 );
 
-create table SEMESTER(
-	ID int auto_increment,
-    NAME char(3),
-    YEAR int,
-    STARTDATE date,
-    ENDDATE date,
-    constraint pk_se primary key(ID),
-    constraint ck_se_st check(YEAR >= 0),
-    constraint ck_se_na check(NAME in ('HK1', 'HK2', 'HK3'))
+create table semester
+(
+    ID        int auto_increment
+        primary key,
+    NAME      char(3) null,
+    YEAR      int     null,
+    STARTDATE date    null,
+    ENDDATE   date    null,
+    constraint ck_se_na
+        check (`NAME` in ('HK1', 'HK2', 'HK3')),
+    constraint ck_se_st
+        check (`YEAR` >= 0)
 );
 
-create table CLASS(
-	ID int auto_increment,
-    NAME nvarchar(100),
-    NUMMALE int default(0),
-    NUMFEMALE int default(0),
-    constraint pk_cl primary key(ID)
+create table courseregistrationsession
+(
+    ID        int auto_increment
+        primary key,
+    STARTDATE date null,
+    ENDDATE   date null,
+    IDSE      int  null,
+    constraint fk_crs_se
+        foreign key (IDSE) references semester (ID)
 );
 
-create table STUDENT(
-	ID int auto_increment,
-    USERNAME varchar(30),
-    PASSWORD varchar(30),
-	CODE char(10),
-    NAME nvarchar(100),
-    SEX nvarchar(3),
-    BIRTH date,
-    NUMSUBJECT int default(0),
-    IDCL int,
-    constraint pk_sv primary key (ID),
-    constraint fk_sv_cl foreign key(IDCL) references CLASS(ID),
-    constraint ck_sv check(SEX in ('Nam', 'Nữ'))
+create table student
+(
+    ID         int auto_increment
+        primary key,
+    USERNAME   varchar(30)               null,
+    PASSWORD   varchar(30)               null,
+    CODE       char(10)                  null,
+    NAME       varchar(100) charset utf8 null,
+    SEX        varchar(3) charset utf8   null,
+    BIRTH      date                      null,
+    NUMSUBJECT int default 0             null,
+    IDCL       int                       null,
+    constraint fk_sv_cl
+        foreign key (IDCL) references class (ID),
+    constraint ck_sv
+        check (`SEX` in ('Nam', 'Nữ'))
 );
 
-create table COURSEREGISTRATIONSESSION(
-	ID int auto_increment,
-    STARTDATE date,
-    ENDDATE date,
-    IDSE int,
-    constraint pk_crs primary key(ID),
-    constraint fk_crs_se foreign key(IDSE) references SEMESTER(ID)
+create table subject
+(
+    ID        int auto_increment
+        primary key,
+    CODE      char(10)                  null,
+    NAME      varchar(100) charset utf8 null,
+    NUMCREDIT int                       null
 );
 
-create table COURSE(
-	ID int auto_increment,
-    NAME nvarchar(100),
-    YEAR int,
-    TEACHER nvarchar(100),
-    ROOM char(10),
-    DAY nvarchar(10),
-    SHIFT int,
-    IDSU int,
-    IDMI int,
-    IDCRS int,
-    constraint pk_co primary key(ID),
-    constraint ck_co_day check(DAY in ('Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7')),
-	constraint ck_co_sh check(SHIFT in (1, 2, 3, 4)),
-    constraint fk_co_su foreign key(IDSU) references SUBJECT(ID),
-    constraint fk_co_mi foreign key(IDMI) references MINISTRY(ID),
-    constraint fk_co_crs foreign key(IDCRS) references COURSEREGISTRATIONSESSION(ID)
+create table course
+(
+    ID      int                       not null
+        primary key,
+    NAME    varchar(100) charset utf8 null,
+    YEAR    int                       null,
+    TEACHER varchar(100) charset utf8 null,
+    ROOM    char(10)                  null,
+    DAY     varchar(10) charset utf8  null,
+    SHIFT   int                       null,
+    IDSU    int                       null,
+    IDMI    int                       null,
+    IDCRS   int                       null,
+    constraint fk_co_crs
+        foreign key (IDCRS) references courseregistrationsession (ID),
+    constraint fk_co_mi
+        foreign key (IDMI) references ministry (ID),
+    constraint fk_co_su
+        foreign key (IDSU) references subject (ID),
+    constraint ck_co_day
+        check (`DAY` in ('Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7')),
+    constraint ck_co_sh
+        check (`SHIFT` in (1, 2, 3, 4))
 );
 
-
-create table COURSEREGISTRATION (
-	ID int auto_increment,
-    IDSV int,
-    IDCO int,
-    REGISTRAIONDATE datetime,
-    constraint pk_cr primary key(ID),
-    constraint fk_cr_sv foreign key(IDSV) references STUDENT(ID),
-    constraint fk_cr_co foreign key(IDCO) references COURSE(ID)
+create table courseregistration
+(
+    ID              int auto_increment
+        primary key,
+    IDSV            int      null,
+    IDCO            int      null,
+    REGISTRAIONDATE datetime null,
+    constraint fk_cr_co
+        foreign key (IDCO) references course (ID),
+    constraint fk_cr_sv
+        foreign key (IDSV) references student (ID)
 );
 
-insert into CLASS(ID, NAME) values(1, '18CTT1');
-insert into CLASS(ID, NAME) values(2, '18CTT2');
-insert into CLASS(ID, NAME) values(3, '18CTT3');
+INSERT INTO course_registration_system.class (ID, NAME, NUMMALE, NUMFEMALE) VALUES (1, '18CTT1', 1, 0);
+INSERT INTO course_registration_system.class (ID, NAME, NUMMALE, NUMFEMALE) VALUES (2, '18CTT2', 0, 0);
+INSERT INTO course_registration_system.class (ID, NAME, NUMMALE, NUMFEMALE) VALUES (3, '18CTT3', 0, 0);
 
-insert into STUDENT values(1, 'votheminh', 'votheminh', '18120211', 'Võ Thế Minh', 'Nam' , str_to_date('16/11/2000', '%d/%m/%Y'), 0, 2);
-insert into STUDENT values(2, 'nguyenthuyduong', 'nguyenthuyduong', '31052000', 'Nguyễn Thùy Dương', 'Nữ', '2000/5/31', 0, 1);
-insert into STUDENT values(3, 'nguyenbaolong', 'nguyenbaolong', '18120210', 'Nguyễn Bảo Long', 'Nam', '2000/7/14', 0, 3);
+INSERT INTO course_registration_system.student (ID, USERNAME, PASSWORD, CODE, NAME, SEX, BIRTH, NUMSUBJECT, IDCL) VALUES (1, 'votheminh', 'votheminh', '18120211', 'Võ Thế Minh', 'Nam', '2000-11-16', 2, 2);
+INSERT INTO course_registration_system.student (ID, USERNAME, PASSWORD, CODE, NAME, SEX, BIRTH, NUMSUBJECT, IDCL) VALUES (2, 'nguyenthuyduong', 'nguyenthuyduong', '31052000', 'Nguyễn Thùy Dương', 'Nữ', '2000-05-31', 2, 1);
+INSERT INTO course_registration_system.student (ID, USERNAME, PASSWORD, CODE, NAME, SEX, BIRTH, NUMSUBJECT, IDCL) VALUES (3, 'nguyenbaolong', 'nguyenbaolong', '18120210', 'Nguyễn Bảo Long', 'Nam', '2000-07-14', 1, 3);
 
-insert into MINISTRY values(1, "ministry1", "ministry1", "Trần Bạch Huệ");
-insert into MINISTRY values(2, "ministry2", "ministry2", "Võ Thế Hùng");
-insert into MINISTRY values(3, "ministry3", "ministry3", "Võ Thảo Trang");
+INSERT INTO course_registration_system.ministry (ID, USERNAME, PASSWORD, NAME) VALUES (1, 'ministry1', 'ministry1', 'Trần Bạch Huệ');
+INSERT INTO course_registration_system.ministry (ID, USERNAME, PASSWORD, NAME) VALUES (2, 'ministry2', 'ministry2', 'Võ Thế Hùng');
+INSERT INTO course_registration_system.ministry (ID, USERNAME, PASSWORD, NAME) VALUES (3, 'ministry3', 'ministry3', 'Võ Thảo Trang');
 
-insert into SUBJECT values(1, "CTDL&GT", "Cấu trúc dữ liệu và giải thuật", 4);
-insert into SUBJECT values(2, "OOP", "Lập trình hướng đối tượng", 4);
-insert into SUBJECT values(3, "PTUDW", "Phát triển ứng dụng web", 4);
+INSERT INTO course_registration_system.semester (ID, NAME, YEAR, STARTDATE, ENDDATE) VALUES (1, 'HK1', 2021, '2021-10-01', '2022-01-01');
+INSERT INTO course_registration_system.semester (ID, NAME, YEAR, STARTDATE, ENDDATE) VALUES (2, 'HK2', 2020, '2022-02-01', '2022-06-01');
+INSERT INTO course_registration_system.semester (ID, NAME, YEAR, STARTDATE, ENDDATE) VALUES (3, 'HK3', 2020, '2022-07-01', '2022-09-01');
 
-insert into COURSE values(1, "")
+INSERT INTO course_registration_system.subject (ID, CODE, NAME, NUMCREDIT) VALUES (1, 'CTDL&GT', 'Cấu trúc dữ liệu và giải thuật', 4);
+INSERT INTO course_registration_system.subject (ID, CODE, NAME, NUMCREDIT) VALUES (2, 'OOP', 'Lập trình hướng đối tượng', 4);
+INSERT INTO course_registration_system.subject (ID, CODE, NAME, NUMCREDIT) VALUES (3, 'PTUDW', 'Phát triển ứng dụng web', 4);
 
--- insert into COURSE values(1, 'CTDL', 2020);
--- insert into COURSE values(2, 'OOP', 2020);
--- insert into COURSE values(3, 'Android', 2020);
+INSERT INTO course_registration_system.courseregistrationsession (ID, STARTDATE, ENDDATE, IDSE) VALUES (1, '2021-09-01', '2021-10-01', 1);
+INSERT INTO course_registration_system.courseregistrationsession (ID, STARTDATE, ENDDATE, IDSE) VALUES (2, '2022-01-01', '0202-02-01', 2);
+INSERT INTO course_registration_system.courseregistrationsession (ID, STARTDATE, ENDDATE, IDSE) VALUES (3, '2022-06-01', '2022-07-01', 3);
 
--- insert into COURSEREGISTRATION values (1, 1, 1, '2020/11/16 18:00:00');
--- insert into COURSEREGISTRATION values (2, 1, 2, '2020/11/16 18:00:00');
--- insert into COURSEREGISTRATION values (3, 1, 3, '2020/11/16 18:00:00');
+INSERT INTO course_registration_system.course (ID, NAME, YEAR, TEACHER, ROOM, DAY, SHIFT, IDSU, IDMI, IDCRS) VALUES (1, 'CTDL&GL 18CTT2', 2021, 'Phan Thị Phương Uyền', 'E104', 'Thứ 2', 1, 1, 1, 1);
+INSERT INTO course_registration_system.course (ID, NAME, YEAR, TEACHER, ROOM, DAY, SHIFT, IDSU, IDMI, IDCRS) VALUES (2, 'OOP 18CTT1', 2020, 'Nguyễn Văn Khiêst', 'F203', 'Thứ 3', 3, 2, 2, 1);
+INSERT INTO course_registration_system.course (ID, NAME, YEAR, TEACHER, ROOM, DAY, SHIFT, IDSU, IDMI, IDCRS) VALUES (3, 'PTUDWEB 18CTT2', 2020, 'Ngô Ngọc Đăng Khoa', 'F105', 'Thứ 4', 3, 3, 3, 1);
 
 
-
+INSERT INTO course_registration_system.courseregistration (ID, IDSV, IDCO, REGISTRAIONDATE) VALUES (1, 1, 1, '2021-09-15 10:00:00');
+INSERT INTO course_registration_system.courseregistration (ID, IDSV, IDCO, REGISTRAIONDATE) VALUES (2, 1, 2, '2021-09-16 10:00:00');
+INSERT INTO course_registration_system.courseregistration (ID, IDSV, IDCO, REGISTRAIONDATE) VALUES (5, 2, 1, '2021-09-15 10:00:00');
+INSERT INTO course_registration_system.courseregistration (ID, IDSV, IDCO, REGISTRAIONDATE) VALUES (6, 2, 2, '2021-09-15 10:00:00');
+INSERT INTO course_registration_system.courseregistration (ID, IDSV, IDCO, REGISTRAIONDATE) VALUES (8, 3, 1, '2021-09-15 10:00:00');
 
